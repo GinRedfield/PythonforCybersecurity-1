8912178bf9db988a15ed44a2a9aa8c3e7ee86aae
Rain <Toss781929@gmail.com>
2023-03-21 15:08:08 -0700

    test2

---
+++ practice/ch2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# python3 ch2.py
+# or sudo chmod +x ch2.py + ./ch2.py
+message_1 = "Hello"
+num = 12
+message_2 = "Please enter a num >> "
+print(message_1,12)
+val = input(message_2)
+print("{0:.2f} DOLLAR!".format(float(val)))
---
+++ practice/ch3.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+import platform
+import os
+os.system('ping -c 1 -w 2 8.8.8.8')
